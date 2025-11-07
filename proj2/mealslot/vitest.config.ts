dc9e08e722d861bf51f3a622b9bd19eae6f18031
Ramen-Noodle <clee9g0@gmail.com>
2025-11-06 23:13:24 -0500

    update config for coverage

--- proj2/mealslot/vitest.config.ts
+++ proj2/mealslot/vitest.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     environment: "happy-dom",
     setupFiles: ["./tests/setupTests.ts"],
     globals: true,
+    coverage: {
+      reporter: ['text', 'lcov'], // lcov file will be written to coverage/lcov.info
+      // optionally: provider: 'c8' or other settings
+    },
   },
 });
